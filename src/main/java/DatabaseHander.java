6771b99f82c1655b8fbc62aed6401e5e000e052c
it_hoppenz <julienhoppenz13@gmail.com>
2024-10-17 15:12:46 +0200

    Database Connection works now.

--- src/main/java/DatabaseHander.java
+++ src/main/java/DatabaseHander.java
@@ -1,2 +1,63 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class DatabaseHander {
+
+        // JDBC driver name and database URL
+
+        static final String JDBC_DRIVER = "org.mariadb.jdbc.Driver";
+        static final String DB_URL = "jdbc:mariadb://127.0.0.1:3360/db";
+
+        //  Database credentials
+        static final String USER = "root";
+        static final String PASS = "12345";
+
+        public static void main(String[] args) {
+            Connection conn = null;
+            Statement stmt = null;
+            try {
+                //STEP 2: Register JDBC driver
+                Class.forName("org.mariadb.jdbc.Driver");
+
+                //STEP 3: Open a connection
+                System.out.println("Connecting to a selected database...");
+                conn = DriverManager.getConnection(
+                        "jdbc:mariadb://127.0.0.1:3360/db", "root", "12345");
+                System.out.println("Connected database successfully...");
+
+                //STEP 4: Execute a query
+                System.out.println("Creating table in given database...");
+                stmt = conn.createStatement();
+
+                String sql = "CREATE TABLE TEST";
+
+                stmt.executeUpdate(sql);
+                System.out.println("Created table in given database...");
+            } catch (SQLException se) {
+                //Handle errors for JDBC
+                se.printStackTrace();
+            } catch (Exception e) {
+                //Handle errors for Class.forName
+                e.printStackTrace();
+            } finally {
+                //finally block used to close resources
+                try {
+                    if (stmt != null) {
+                        conn.close();
+                    }
+                } catch (SQLException se) {
+                }// do nothing
+                try {
+                    if (conn != null) {
+                        conn.close();
+                    }
+                } catch (SQLException se) {
+                    se.printStackTrace();
+                }//end finally try
+            }//end try
+            System.out.println("Goodbye!");
+        }//end main
+
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,32 +1,44 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class Main {
-
-    public static void main(String[] args) throws SQLException {
+    DatabaseHander dbHandler = new DatabaseHander();
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
         new Main().doit();
     }
 
 
 
 //doit um aus statischem rauszukommen
-    void doit() throws SQLException {
-    getDatabaseConn();
+    void doit() throws SQLException, ClassNotFoundException {
+       // dbHandler.
+//        System.out.println("START, printing string of sql");
+//        System.out.println(String.valueOf(getClass().getClassLoader().getResource("C:\\Users\\it_hoppenz\\IdeaProjects\\3FA071_Gruppe_6\\src\\main" +
+//                "\\resources\\databasemigrations\\V1__customer-schema.txt")));
+        System.out.println(getClass().getClassLoader().getResource("")
+        );
+        getDatabaseConn();
 
 
     }
 
 
-    void getDatabaseConn() throws SQLException {
+    void getDatabaseConn() throws SQLException, ClassNotFoundException {
+        Class.forName("org.mariadb.jdbc.Driver");
         Connection connection = DriverManager.
-                getConnection("jdbc:mariadb://127.0.0.1:3306/DB?user="
-                        + Constants.username + "&password=" + Constants.password);
+                getConnection("jdbc:mariadb://127.0.0.1:3306/", Constants.username, Constants.password);
+                      //  + Constants.username + "&password=" + Constants.password);
 
         Statement stmt = connection.createStatement();
-        stmt.executeUpdate(String.valueOf(getClass().getClassLoader().getResource("C:\\Users\\it_hoppenz\\IdeaProjects\\3FA071_Gruppe_6\\src\\main" +
-                "\\resources\\databasemigrations\\V1__customer-schema.sql")));
+        stmt.executeUpdate("use test");
+        String selectSQL = "SELECT * FROM testtable";
+        ResultSet rs = stmt.executeQuery(selectSQL);
+        while(rs.next()){
+            String value = rs.getString("spalte1");
+
+            System.out.println("spalte1: " + value);
+        }
+       // stmt.executeUpdate(String.valueOf(getClass().getClassLoader().getResource("C:\\Users\\it_hoppenz\\IdeaProjects\\3FA071_Gruppe_6\\src\\main" +
+        //        "\\resources\\databasemigrations\\V1__customer-schema.sql")));
         stmt.close();
         connection.close();
     }
