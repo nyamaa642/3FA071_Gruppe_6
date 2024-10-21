9ed852aac7284d6091f2eed82c8f25748f565205
Nyambayar Ganbat <ng@safri.net>
2024-10-21 09:50:03 +0200

    Fix the V1 database migration

---
+++ src/main/resources/databasemigrations/V1__kunden-schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE public."customer" (
+    id uuid NOT NULL,
+    firstname character varying(100) NOT NULL,
+    lastname character varying(100) NOT NULL,
+    birthDate date NOT NULL,
+    primary key(id)
+)
