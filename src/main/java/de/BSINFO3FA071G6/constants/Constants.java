033828a71a8c4cd11af9d441eb3bc6c8b0e6e513
it_hoppenz <julien.hoppenz@develey.de>
2025-01-20 08:46:06 +0100

    added Properties file for database connection

--- src/main/java/de/BSINFO3FA071G6/constants/Constants.java
+++ src/main/java/de/BSINFO3FA071G6/constants/Constants.java
@@ -3,8 +3,8 @@ package de.BSINFO3FA071G6.constants;
 public class Constants
 {
     public static String databaseName = "test";
-    static String databaseUrl = "jdbc:mariadb://127.0.0.1:3306/";
-    static String username = "root";
-    static String password = "12345";
+    public static String databaseUrl = "jdbc:mariadb://127.0.0.1:3306/";
+    public static String username = "root";
+    public static String password = "12345";
 
 }
