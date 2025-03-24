fccfce1cd3226b09aa1549f242bd84e70670ba8f
Nyambayar Ganbat <ng@safri.net>
2025-03-24 13:22:07 +0100

    Specify the database for the connection

--- src/main/java/api/database/Constants.java
+++ src/main/java/api/database/Constants.java
@@ -2,7 +2,7 @@ package api.database;
 
 public class Constants {
     static String databaseName = "test";
-    public static String databaseUrl = "jdbc:mariadb://127.0.0.1:3306/";
+    public static String databaseUrl = "jdbc:mariadb://127.0.0.1:3306/test";
     public static String username = "root";
     public static String password = "12345";
     public static String jUnitTestDb = "TestJUnitDB";
